fb05c9c57c25f769372121770be31535639e8cd0
SamiWilliamsZuto <samantha.williams@zuto.com>
2022-08-04 16:25:25 +0100

    Grid displays random letters, added list of words

--- WordSearch/WpfApp1/MainWindow.xaml.cs
+++ WordSearch/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -10,10 +11,14 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int MaxWords = 4;
+        private readonly string[] _words = { "Cat", "Dog", "Worm", "Sausage", "Potato", "Porridge", "Towel", "Rotisserie", "Gym", "Tinder" };
+        
         public MainWindow()
         {
             InitializeComponent();
 
+            CreateLabels();
             CreateButtons();
         }
 
@@ -30,7 +35,7 @@ namespace WpfApp1
                     {
                         Name = $"button{x}{y}",
                         Tag = $"button {x},{y}",
-                        Content = "A",
+                        Content = GenerateRandomChar(),
                         Height = buttonHeight,
                         Width = buttonWidth,
                         Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
@@ -40,11 +45,44 @@ namespace WpfApp1
                     btn.Click += button_Click;
 
                     ButtonCanvas.Children.Add(btn);
-                    Canvas.SetLeft(btn, x * buttonWidth);
-                    Canvas.SetTop(btn, y * buttonHeight);
+                    Canvas.SetLeft(btn, x * buttonWidth + 10);
+                    Canvas.SetTop(btn, y * buttonHeight + 10);
                 }
             }
         }
+
+        public void CreateLabels()
+        {
+            var wordIds = new Tuple<float, string>[_words.Length];
+            
+            for (int i = 0; i < wordIds.Length; i++)
+            {
+                wordIds[i] = new Tuple<float, string>(new Random().NextSingle(), _words[i]);
+            }
+            
+            Array.Sort(wordIds);
+            
+            for (int i = 0; i < (wordIds.Length < MaxWords ? wordIds.Length : MaxWords); i++)
+            {
+                var label = new Label()
+                {
+                    Name = wordIds[i].Item2.ToUpper(),
+                    Content = wordIds[i].Item2.ToUpper()
+                };
+
+                ButtonCanvas.Children.Add(label);
+                Canvas.SetLeft(label, 845);
+                Canvas.SetTop(label, i * 14);
+            }
+        }
+        
+
+        private char GenerateRandomChar()
+        {
+            var randomChar = new Random().Next(65, 90 + 1);
+            return (char) randomChar;
+        }
+
         void button_Click(object sender, RoutedEventArgs e)
         {
             Console.WriteLine($"You clicked on the {(sender as Button)?.Tag}. button.");
